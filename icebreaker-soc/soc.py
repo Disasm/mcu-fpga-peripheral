800b170a01439202a01415258bfa620623fee92a
Vadim Kaushan <admin@disasm.info>
2020-10-28 10:46:13 +0300

    Add SoC code

---
+++ icebreaker-soc/soc.py
@@ -0,0 +1,206 @@
+#!/usr/bin/env python3
+
+# This file is Copyright (c) 2019 Sean Cross <sean@xobs.io>
+# This file is Copyright (c) 2018 David Shah <dave@ds0.me>
+# This file is Copyright (c) 2020 Piotr Esden-Tempski <piotr@esden.net>
+# This file is Copyright (c) 2020 Vadim Kaushan <admin@disasm.info>
+# License: BSD
+
+# This target was originally based on the Fomu target.
+
+# This variable defines all the external programs that this module
+# relies on.  lxbuildenv reads this variable in order to ensure
+# the build will finish without exiting due to missing third-party
+# programs.
+LX_DEPENDENCIES = ["riscv", "icestorm", "yosys", "nextpnr-ice40"]
+
+# Import lxbuildenv to integrate the deps/ directory
+import lxbuildenv
+
+import argparse
+
+from litex.build.generic_platform import Subsignal, Pins, IOStandard
+from litex.build.lattice.programmer import IceStormProgrammer
+from litex.soc.cores.up5kspram import Up5kSPRAM
+from litex.soc.integration.builder import Builder, builder_args, builder_argdict
+from litex.soc.integration.soc_core import soc_core_args, soc_core_argdict, SoCCore
+from litex_boards.platforms.icebreaker import Platform, break_off_pmod
+from migen import *
+from migen.genlib.resetsync import AsyncResetSynchronizer
+from litex.soc.cores.clock import iCE40PLL
+from litex.soc.integration.doc import AutoDoc
+
+from leds import Leds
+from spi_slave import SPIBridge
+
+
+class _CRG(Module, AutoDoc):
+    """Icebreaker Clock Resource Generator
+
+    The system is clocked by the external 12MHz clock. But if a sys_clk_freq is set to a value
+    that is different from the default 12MHz we will feed it through the PLL block and try to
+    generate a clock as close as possible to the selected frequency.
+    """
+    def __init__(self, platform, sys_clk_freq):
+        self.clock_domains.cd_sys = ClockDomain()
+        self.clock_domains.cd_por = ClockDomain()
+
+        # # #
+
+        # Clocks
+        clk12 = platform.request("clk12")
+        if sys_clk_freq == 12e6:
+            self.comb += self.cd_sys.clk.eq(clk12)
+        else:
+            self.submodules.pll = pll = iCE40PLL(primitive="SB_PLL40_PAD")
+            pll.register_clkin(clk12, 12e6)
+            pll.create_clkout(self.cd_sys, sys_clk_freq, with_reset=False)
+        platform.add_period_constraint(self.cd_sys.clk, 1e9 / sys_clk_freq)
+
+        # Power On Reset
+        self.reset = Signal()
+        por_cycles = 8
+        por_counter = Signal(log2_int(por_cycles), reset=por_cycles - 1)
+        self.comb += self.cd_por.clk.eq(self.cd_sys.clk)
+        platform.add_period_constraint(self.cd_por.clk, 1e9 / sys_clk_freq)
+        self.sync.por += If(por_counter != 0, por_counter.eq(por_counter - 1))
+        self.comb += self.cd_sys.rst.eq(por_counter != 0)
+        self.specials += AsyncResetSynchronizer(self.cd_por, self.reset)
+
+
+# BaseSoC ------------------------------------------------------------------------------------------
+
+class BaseSoC(SoCCore):
+    """A SoC on iCEBreaker, without a softcore CPU"""
+
+    # Statically-define the memory map, to prevent it from shifting across various litex versions.
+    SoCCore.mem_map = {
+        "csr":              0x00000000,
+        "sram":             0x00020000,
+    }
+
+    def __init__(self, sys_clk_freq, **kwargs):
+        platform = Platform()
+
+        kwargs["cpu_type"] = None
+        kwargs["with_uart"] = False
+        kwargs["with_timer"] = False
+        #kwargs["with_ctrl"] = False
+
+        # Force the SRAM size to 0, because we add our own SRAM with SPRAM
+        kwargs["integrated_sram_size"] = 0
+        kwargs["integrated_rom_size"] = 0
+
+        kwargs["csr_data_width"] = 32
+
+        SoCCore.__init__(self, platform, sys_clk_freq, **kwargs)
+
+        self.submodules.crg = _CRG(platform, sys_clk_freq)
+
+        reset_btn = platform.request("user_btn_n")
+        self.comb += self.crg.reset.eq(~reset_btn)
+
+        led = platform.request("user_led_n")
+        led2 = platform.request("user_led_n")
+
+        spi_ext = [
+            ("spi_slave", 0,
+                Subsignal("cs_n", Pins("PMOD1A:0"), IOStandard("LVCMOS33")),
+                Subsignal("clk", Pins("PMOD1A:1"), IOStandard("LVCMOS33")),
+                Subsignal("miso", Pins("PMOD1A:2"), IOStandard("LVCMOS33")),
+                Subsignal("mosi", Pins("PMOD1A:3"), IOStandard("LVCMOS33")),
+            ),
+        ]
+        platform.add_extension(spi_ext)
+        spi_pads = platform.request("spi_slave")
+
+        self.submodules.bridge = bridge = SPIBridge(spi_pads)
+        self.bus.add_master(name="bridge", master=self.bridge.wishbone)
+
+        # UP5K has single port RAM, which is a dedicated 128 kilobyte block.
+        # Use this as CPU RAM.
+        # spram_size = 128 * 1024
+        # self.submodules.spram = Up5kSPRAM(size=spram_size)
+        # self.register_mem("sram", self.mem_map["sram"], self.spram.bus, spram_size)
+
+        #####################################################
+        # DEBUG
+        #####################################################
+
+        busy = self.bridge.wishbone.cyc
+        self.comb += led.eq(~busy)
+
+        busy_locked = Signal()
+        self.sync += If(busy, busy_locked.eq(1))
+        self.comb += led2.eq(~busy_locked)
+
+        # The litex SPI module supports memory-mapped reads, as well as a bit-banged mode
+        # for doing writes.
+        # spiflash_size = 16 * 1024 * 1024
+        # self.submodules.spiflash = SpiFlash(platform.request("spiflash4x"), dummy=6, endianness="little")
+        # self.register_mem("spiflash", self.mem_map["spiflash"], self.spiflash.bus, size=spiflash_size)
+        # self.add_csr("spiflash")
+
+        # Add ROM linker region
+        #self.add_memory_region("rom", self.mem_map["spiflash"] + flash_offset, spiflash_size - flash_offset, type="cached+linker")
+
+        platform.add_extension(break_off_pmod)
+        self.submodules.leds = Leds(Cat(
+            #platform.request("user_ledr_n"),
+            #platform.request("user_ledg_n"),
+            platform.request("user_ledr"),
+            platform.request("user_ledg", 0),
+            platform.request("user_ledg", 1),
+            platform.request("user_ledg", 2),
+            platform.request("user_ledg", 3)),
+            led_polarity=0x00,
+            led_name=[
+                #["ledr", "The Red LED on the main iCEBreaker board."],
+                #["ledg", "The Green LED on the main iCEBreaker board."],
+                ["hledr1", "The center Red LED #1 on the iCEBreaker head."],
+                ["hledg2", "Green LED #2 on the iCEBreaker head."],
+                ["hledg3", "Green LED #3 on the iCEBreaker head."],
+                ["hledg4", "Green LED #4 on the iCEBreaker head."],
+                ["hledg5", "Green LED #5 on the iCEBreaker head."]])
+
+        self.add_csr("leds")
+
+        assert hasattr(self.platform.toolchain, "build_template")
+        if self.platform.toolchain.build_template[0].startswith("yosys "):
+            self.platform.toolchain.build_template[0] =\
+                self.platform.toolchain.build_template[0].replace("yosys ", "yosys -q ")
+
+
+def main():
+    parser = argparse.ArgumentParser(description="LiteX SoC on iCEBreaker")
+    parser.add_argument("--sys-clk-freq", type=float, default=48e6, help="Select system clock frequency")
+    parser.add_argument("--document-only", action="store_true", help="Do not build a soc. Only generate documentation.")
+    parser.add_argument("--flash", action="store_true", help="Load bitstream")
+    builder_args(parser)
+    soc_core_args(parser)
+    args = parser.parse_args()
+
+    # Create the SOC
+    soc = BaseSoC(sys_clk_freq=int(args.sys_clk_freq), **soc_core_argdict(args))
+
+    # Configure command line parameter defaults
+    # Don't build software -- we don't include it since we just jump to SPI flash.
+    builder_kwargs = builder_argdict(args)
+    builder_kwargs["compile_software"] = False
+
+    if args.document_only:
+        builder_kwargs["compile_gateware"] = False
+    if builder_kwargs["csr_svd"] is None:
+        builder_kwargs["csr_svd"] = "../litex-pac/soc.svd"
+
+    # Create and run the builder
+    builder = Builder(soc, **builder_kwargs)
+    builder.build()
+
+    # If requested load the resulting bitstream onto the iCEBreaker
+    if args.flash:
+        IceStormProgrammer().flash(0x00000000, "build/icebreaker/gateware/icebreaker.bin")
+
+
+if __name__ == "__main__":
+    main()
---
+++ icebreaker-soc/spi_slave.py
@@ -0,0 +1,173 @@
+# Copyright (c) 2019-2020 Florent Kermarrec <florent@enjoy-digital.fr>
+# Copyright (c) 2020 Vadim Kaushan <admin@disasm.info>
+# SPDX-License-Identifier: BSD-2-Clause
+
+from litex.soc.integration.doc import AutoDoc, ModuleDoc
+from litex.soc.interconnect.csr import AutoCSR
+from litex.soc.interconnect import wishbone
+from migen import *
+from migen.genlib.cdc import MultiReg
+
+
+# SPI Slave ----------------------------------------------------------------------------------------
+# This module is a modified version of `litex.soc.cores.spi.SPISlave`
+
+class SPISlave(Module):
+    """4-wire SPI Slave
+
+    Provides a simple and minimal hardware SPI Slave with CPOL=0, CPHA=0 and build time configurable
+    data_width.
+    """
+    pads_layout = [("clk", 1), ("cs_n", 1), ("mosi", 1), ("miso", 1)]
+
+    def __init__(self, pads):
+        if pads is None:
+            pads = Record(self.pads_layout)
+        if not hasattr(pads, "cs_n"):
+            pads.cs_n = Signal()
+        self.pads       = pads
+
+        self.start    = Signal()
+        self.byte     = Signal()
+        self.length   = Signal(3)
+        self.done     = Signal()
+        self.irq      = Signal()
+        self.mosi     = Signal(8)
+        self.miso     = Signal(8)
+        self.cs       = Signal()
+
+        # # #
+
+        clk  = Signal()
+        cs   = Signal()
+        mosi = Signal()
+        miso = Signal()
+
+        # IOs <--> Internal (input resynchronization) ----------------------------------------------
+        self.specials += [
+            MultiReg(pads.clk, clk),
+            MultiReg(~pads.cs_n, cs),
+            MultiReg(pads.mosi, mosi),
+        ]
+        self.comb += pads.miso.eq(miso)
+
+        # Clock detection --------------------------------------------------------------------------
+        clk_d = Signal()
+        clk_rise = Signal()
+        clk_fall = Signal()
+        self.sync += clk_d.eq(clk)
+        self.comb += clk_rise.eq(clk & ~clk_d)
+        self.comb += clk_fall.eq(~clk & clk_d)
+
+        # Control FSM ------------------------------------------------------------------------------
+        self.submodules.fsm = fsm = FSM(reset_state="IDLE")
+        fsm.act("IDLE",
+            If(cs,
+                self.start.eq(1),
+                NextValue(self.length, 0),
+                NextState("XFER")
+            ).Else(
+                self.done.eq(1)
+            )
+        )
+        fsm.act("XFER",
+            If(~cs,
+                self.irq.eq(1),
+                NextState("IDLE")
+            ),
+            NextValue(self.length, self.length + clk_fall)
+        )
+        byte = Signal()
+        self.comb += byte.eq(clk_fall & (self.length == 7))
+        self.sync += self.byte.eq(byte)
+
+        # Master In Slave Out (MISO) generation (generated on spi_clk falling edge) ----------------
+        miso_data = Signal(8)
+        self.sync += \
+            If(self.start | self.byte,
+                miso_data.eq(self.miso)
+            ).Elif(cs & clk_fall,
+                miso_data.eq(Cat(Signal(), miso_data[:-1]))
+            )
+        self.comb += miso.eq(miso_data[-1])
+
+        # Master Out Slave In (MOSI) capture (captured on spi_clk rising edge) ---------------------
+        mosi_data = Signal(8)
+        self.sync += [
+            If(cs & clk_rise,
+                mosi_data.eq(Cat(mosi, mosi_data[:-1]))
+            ),
+            If(byte, self.mosi.eq(mosi_data))
+        ]
+
+
+class SPIBridge(Module, AutoCSR, AutoDoc):
+    def __init__(self, spi_pads):
+        # Documentation
+        self.intro = ModuleDoc("SPI slave driver")
+
+        self.wishbone = bus = wishbone.Interface()
+        self.submodules.spi = spi = SPISlave(spi_pads)
+
+        spi_counter = Signal(4)
+        spi_dword_mosi = Signal(32)
+        spi_dword_miso = Signal(32)
+        self.sync += \
+            If(spi.start,
+                spi_counter.eq(0),
+            ).Elif(spi.byte,
+                spi_counter.eq(spi_counter + 1),
+                spi_dword_mosi.eq(Cat(spi_dword_mosi[8:], spi.mosi)),
+                spi_dword_miso.eq(Cat(spi_dword_miso[8:], Signal(8))),
+            )
+
+        address = Signal(16)
+        address_hi = Signal(16)
+
+        command = Signal(8)
+
+        self.comb += spi.miso.eq(spi_dword_miso[:8])
+
+        self.submodules.fsm = fsm = FSM(reset_state="IDLE")
+        fsm.act("IDLE",
+            If(spi.start, NextState("COMMAND"))
+        )
+        fsm.act("COMMAND",
+            If(spi.done,
+                NextState("IDLE")
+            ).Else(
+                If(spi_counter == 1,
+                    NextValue(command, spi_dword_mosi[-8:]),
+                ),
+                If(spi_counter == 3,
+                    NextValue(address, spi_dword_mosi[-16:]),
+                    If(command == 0x03, NextState("READ"))
+                ),
+                If((spi_counter == 7) and command == 0x02,
+                    #NextValue(data, spi_dword_mosi),
+                    NextState("WRITE"),
+                )
+            ),
+        )
+        fsm.act("READ",
+            bus.cyc.eq(1),
+            bus.stb.eq(1),
+            bus.we.eq(0),
+            bus.adr.eq(Cat(address, address_hi)),
+            bus.sel.eq(2 ** len(bus.sel) - 1),
+            If(bus.ack,
+                NextValue(spi_dword_miso, bus.dat_r),
+                NextState("IDLE")
+            )
+        )
+        fsm.act("WRITE",
+            bus.cyc.eq(1),
+            bus.stb.eq(1),
+            bus.we.eq(1),
+            bus.adr.eq(Cat(address, address_hi)),
+            bus.dat_w.eq(spi_dword_mosi),
+            bus.sel.eq(2 ** len(bus.sel) - 1),
+            If(bus.ack,
+                NextState("IDLE")
+            )
+        )
